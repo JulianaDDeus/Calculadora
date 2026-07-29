387ccc6524564b2a064b5fda28c57d116f6d24e5
Juliana <juliana.ndeus@gmail.com>
2026-07-29 17:47:52 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,33 @@
+tela = document.getElementById('conta')
+tela.value = ""
+
+function escreveConta(caracter){
+    tela.value += caracter
+}
+
+function apagaConta(){
+    tela.value = ""
+}
+
+function conta() {
+    try {
+        let expressao = tela.value;
+
+        expressao = expressao.replaceAll("X", "*");
+        expressao = expressao.replaceAll(",", ".");
+        expressao = expressao.replaceAll("%", "/100");
+
+        let resultado = eval(expressao);
+
+        tela.value = resultado;
+    }
+    catch {
+        tela.value = "Erro";
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    conta();
+  }
+});
